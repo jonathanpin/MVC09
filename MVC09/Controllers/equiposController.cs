efb9cb4d92f9e539fb8dba43a66a0041345c41a3
jonathan <alcidescp@hotmail.com>
2023-04-29 18:40:00 -0600

    Agregar archivos de proyecto.

---
+++ MVC09/Controllers/equiposController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MVC09.Models;
+
+namespace MVC09.Controllers
+{
+    public class equiposController : Controller
+    {
+        private readonly equiposContext _equiposContext;
+        public equiposController(equiposContext equiposContext)
+        {
+            _equiposContext = equiposContext;
+        }
+
+        public IActionResult Index()
+        {
+            var listaMarcas =  (from m in _equiposContext.marcas select m).ToList();
+            ViewData["listaMarcas"] = new SelectList(listaMarcas, "id_marcas", "nombre_marca");
+
+            var listaTipos = (from t in _equiposContext.tipo_equipo select t).ToList();
+            ViewData["listaTipos"] = new SelectList(listaTipos, "id_tipo_equipo", "descripcion");
+
+            var listaEquipos = (from e in _equiposContext.equipos 
+                             join m in _equiposContext.marcas on e.marca_id equals m.id_marcas
+                             join t in _equiposContext.tipo_equipo on e.tipo_equipo_id equals t.id_tipo_equipo
+                             select new
+                             {
+                                 nombre = e.nombre,
+                                 descripcion = e.descripcion,
+                                 marca = m.nombre_marca,
+                                 tipo = t.descripcion
+                             }).ToList();
+            ViewData["listaEquipos"] = listaEquipos;
+            return View();
+        }
+        public IActionResult crearEquipo(equipos nuevoEquipo)
+        {
+            _equiposContext.Add(nuevoEquipo);
+            _equiposContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC09/Models/equiposContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using practicaMVC05.Models;
+
+namespace MVC09.Models
+{
+    public class equiposContext : DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext> options) : base(options)
+        {
+        }
+        public DbSet<equipos> equipos { get; set; }
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<tipo_equipo> tipo_equipo { get; set; }
+    }
+}
